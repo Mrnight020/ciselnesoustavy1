b995c7374bc194fe6963b536352c75b816a6cf72
Mrnight020 <97515527+Mrnight020@users.noreply.github.com>
2023-11-07 20:01:37 +0100

    Add files via upload

---
+++ BintoDecDectoBin/hodina6.11/hodina6.11/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace hodina6._11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(63, 42);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "DecToBin";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(285, 145);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(41, 139);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(176, 27);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(63, 201);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 55);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "BinToDec";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 301);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ BintoDecDectoBin/hodina6.11/hodina6.11/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hodina6._11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar > '0' || e.KeyChar < '9' || e.KeyChar != 8))
+            {
+                
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = " ";
+            DecToBin(textBox1, label1);
+            
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = " ";
+            BinToDec(textBox1, label1);
+        }
+
+        void BinToDec(TextBox textbox, Label label)
+        {
+            string kod = textbox.Text;
+            bool jebinarni = true;
+            for(int a = 0; a < kod.Length; a++)
+            {
+                if (kod[a] != '0' && kod[a] != '1')
+                {
+                    jebinarni = false;
+                    break;
+                }
+                   
+            }
+            try
+            {
+                int deckod = 0;
+                int i = 0;
+                if (jebinarni)
+                {
+                    for (int mocnina = kod.Length - 1; mocnina >= 0; mocnina--, i++)
+                    {
+                        int nulajedna = kod[i] - '0';
+
+                        checked { deckod += nulajedna * Convert.ToInt32(Math.Pow(2, mocnina)); }
+                            
+                    }
+
+                    label.Text = deckod.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Zadej platný binarni kod !!");
+                }
+            }
+            catch(OverflowException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+            
+        }
+        void DecToBin(TextBox textbox,Label label)
+        {
+            List<int>list = new List<int>();
+
+            if (int.TryParse(textbox.Text, out int cislo))
+            {
+                if(cislo <= 0)
+                {
+                    MessageBox.Show("Zadej vetsi cislo !!");
+                }
+                else
+                {
+                    for (; cislo > 0; cislo /= 2)
+                    {
+                        list.Add(cislo % 2);
+                    }
+                    list.Reverse();
+                    foreach (int i in list)
+                    {
+                        label.Text += i;
+                    }
+                }
+               
+            }
+            else
+            {
+                MessageBox.Show("Chyba");
+            }
+        }
+    }
+}
